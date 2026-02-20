cfe9e05a25c08caa1d715a37bf58b44ccae6ee4d
Md. Monjurul Haque <himel452@gmail.com>
2026-02-20 23:45:27 +0600

    updated file and add new file

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'mh-academy-v2';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(res => {
+      return res || fetch(evt.request);
+    })
+  );
+});
