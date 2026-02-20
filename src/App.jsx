7331562b69d8493efb539d4d2a9edda14c1ed442
Md. Monjurul Haque <himel452@gmail.com>
2026-02-21 00:04:47 +0600

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import mind from './mind'
+import Mind from './Mind'
 
 function App() {
   return (
     <div>
       {/* আমাদের তৈরি করা গেমের কম্পোনেন্ট এখানে কল করা হলো */}
-      <mind />
+      <Mind />
     </div>
   )
 }
